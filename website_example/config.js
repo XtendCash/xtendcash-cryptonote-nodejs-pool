54e97448bf7498146c35a1f2012dadfe5cdc950d
XtendCash <49289946+XtendCash@users.noreply.github.com>
2019-04-16 11:52:55 +0300

    Initial Commit

---
+++ website_example/config.js
@@ -0,0 +1,13 @@
+var api = "https://poolhost:8119";
+
+var email = "team@xtendcash.com";
+var telegram = "https://t.me/xtendcash";
+var discord = "https://discord.gg/uKSfXpS";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://explorer.xtendcash.com/block/{id}";
+var transactionExplorer = "https://explorer.xtendcash.com/tx/{id}";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
