0d3ed7c4ad2622389962928f8b7d43f489d66122
XtendCash <49289946+XtendCash@users.noreply.github.com>
2019-09-14 00:39:17 +0300

    Update config.js

--- website_example/config.js
+++ website_example/config.js
@@ -1,15 +1,15 @@
-var api = "https://swap.example.com:8119";
+var api = "https://pool.xtendcash.com:8119";
 
 var coinDecimalPlaces = 8;
 
-var email = "";
-var telegram = "";
-var discord = "https://discord.gg/XAmdwSd";
+var email = "team@xtendcash.com";
+var telegram = "https://t.me/xtendcash";
+var discord = "https://discord.gg/uKSfXpS";
 
 var marketCurrencies = ["{symbol}-BTC"];
 
-var blockchainExplorer = "https://explorer.xwp.one/block/{id}";
-var transactionExplorer = "https://explorer.xwp.one/tx/{id}";
+var blockchainExplorer = "https://explorer.xtendcash.com/block/{id}";
+var transactionExplorer = "https://explorer.xtendcash.com/tx/{id}";
 
 var themeCss = "themes/moon_gray.css";
 var themeCssDark = "themes/moon_gray_dark.css";
